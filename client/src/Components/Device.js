9d51ef35b1368a00aaeff0c2b4d4162774584430
Ben Gianni <bgianni@bens-mbp.lan>
2022-08-31 19:30:39 -0400

    working skeleton

--- client/src/Components/Device.js
+++ client/src/Components/Device.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
-export default function Device() {
+export default function Device({mac,hostname,ip}) {
     return (
         <div className="device card">
             <img className="card-img-top" src="" alt="" />
             <div className="card-body">
-                <h5 className="card-title">Device Hostname</h5>
+                <h5 className="card-title">{hostname}</h5>
                 <table className="table">
                     <thead>
-                        <th>Type</th>
-                        <th>Ip</th>
+                        <tr>
+                            <th>Type</th>
+                        </tr>
+                        <tr>
+                            <th>{ip}</th>
+                        </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>type here</td>
-                            <td>ip here</td>
+                            <td>{mac}</td>
                         </tr>
                     </tbody>
                 </table>
--- client/src/Components/DeviceTable.js
+++ client/src/Components/DeviceTable.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import Device from './Device'
 
 export default function DeviceTable() {
+
+    const [deviceList, setDeviceList] = useState([]);
+
+    useEffect(() => {
+        fetch("/api")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data[0].hostname);
+                setDeviceList(data);
+            });
+    }, []);
     return (
         <Container>
-            <Row>
-                <Col xs={12} sm={12} md={6} lg={4} xl={3} className="mb-2">
-                    <Device />
-                </Col>
-                <Col xs={12} sm={12} md={6} lg={4} xl={3} className="mb-2">
-                    <Device />
-                </Col>
-                <Col xs={12} sm={12} md={6} lg={4} xl={3} className="mb-2">
-                    <Device />
-                </Col>
-                <Col xs={12} sm={12} md={6} lg={4} xl={3} className="mb-2">
-                    <Device />
-                </Col>
-                <Col xs={12} sm={12} md={6} lg={4} xl={3} className="mb-2">
-                    <Device />
-                </Col>
-            </Row>
+            {
+                (deviceList.length === 0) ?
+                    <p>loading...</p> :
+                    <Row>
+                        {deviceList.map(device => 
+                        <Col xs={12} sm={12} md={6} lg={4} xl={3} className="mb-2">
+                            <Device mac={device.mac} hostname={device.hostname} ip={device.ip} />
+                        </Col>
+                        )}
+                    </Row>
+            }
         </Container>
     )
 }
